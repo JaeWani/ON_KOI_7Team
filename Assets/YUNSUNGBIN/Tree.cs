73c41c9042c6679265bdbbe15a2dbc732d741e91
yunsungbin <dbstmdqls0211@gmail.com>
2022-08-18 21:18:03 +0900

    08-18

--- Assets/YUNSUNGBIN/Tree.cs
+++ Assets/YUNSUNGBIN/Tree.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                
+            }
+        }
     }
 }
